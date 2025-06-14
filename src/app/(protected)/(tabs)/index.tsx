7acb7959cf7cc2abeaf34f11597cfc86932f750e
Aadi Poddar <aadipoddarmail@gmail.com>
2025-06-14 22:56:47 +0530

    New Post UI

---
+++ src/app/(protected)/(tabs)/index.tsx
@@ -0,0 +1,18 @@
+import PostListItem from "@/components/PostListItem";
+import { dummyPosts } from "@/dummyData";
+import { Link } from "expo-router";
+import { FlatList, Text, View } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <FlatList
+      data={dummyPosts}
+      renderItem={({ item }) => <PostListItem post={item} />}
+      ListHeaderComponent={() => (
+        <Link href="/new" className="text-blue-500 p-4 text-center text-3xl">
+          New Post
+        </Link>
+      )}
+    />
+  );
+}
--- src/app/(tabs)/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function HomeScreen() {
-  return (
-    <View className="flex-1 p-4">
-      <Text className="text-3xl font-bold text-white">Hello World</Text>
-    </View>
-  );
-}
