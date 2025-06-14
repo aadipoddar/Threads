aa7e3d2b81d8ab22b5abf0d2cca95dde1183ce6d
Aadi Poddar <aadipoddarmail@gmail.com>
2025-06-14 20:53:04 +0530

    Path aliases

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -2,8 +2,8 @@ import { Text, View } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text className="text-3xl font-bold text-red-500">Feed</Text>
+    <View className="flex-1 p-4">
+      <Text className="text-3xl font-bold text-white">Hello World</Text>
     </View>
   );
 }
